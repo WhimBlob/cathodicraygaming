1daadb2fc3943782b8b4e395c54f497d1297f1a5
GregHHH <gregoryhaton93@outlook.fr>
2021-03-12 14:54:39 +0100

    debut page profil

--- profil.php
+++ profil.php
@@ -1,7 +1,39 @@
-<?php include 'config/template/head.php'; ?>
+
 <header>
-    <?php include 'config/template/nav.php'; ?>
+    
 </header>
-<h2 class="text-center mt-5 mb-5">Page profil</h2>
+<h2 class="text-center mt-5 mb-5">Bienvenue sur votre espace client, [CLIENT]</h2>
+<button class="profil_button"> Deconnexion </button>
 <hr>
-<?php include 'config/template/footer.php'; ?>
+
+<div class="profil">
+    <h2>MON PROFIL</h2>
+    <button class="profil_button">MODIFIER</button>
+    <div class="profil_content">
+        <label for="">Adresse e-mail</label>
+        <p class="customer_mail">adresse.mail@test.fr</p>
+    </div>
+
+    <div class="profil_content">
+        <label for="">Mot de passe</label>
+        <p class="customer_passwrd">m0t2p4ssM3g4S3cr3t</p>
+    </div>
+
+    <div class="profil_content">
+        <label for="">Nom</label>
+        <p class="customer_name">Marcel Petibonhom</p>
+    </div>
+
+    <div class="profil_content">
+        <label for="">Numéro de téléphone</label>
+        <p class="customer_phone">0621304125</p>
+    </div>
+</div>
+
+<div class="order">
+    <h2>MES COMMANDES</h2>
+
+</div>
+
+
+
