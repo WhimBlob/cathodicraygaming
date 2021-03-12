1b1891ed0129002f5e979507c6c1d96941fd5916
Ughetto Jules <ughetto.jules@gmail.com>
2021-03-12 17:51:18 +0100

    On a les dossiers qui sont prêts, penser à changer ce qu'il faut pour harmoniser, là j'ai mis les headers et footers dans ressources/templates

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
-<?php include 'config/template/head.php'; ?>
-<header>
-    <?php include 'config/template/nav.php'; ?>
-</header>
-<h2 class="text-center mt-5 mb-5">Page Accueil</h2>
-<hr>
-<?php include 'config/template/footer.php'; ?>
+<?php include 'ressources/templates/header.php'?>
+<main>
+  <!-- Hero -->
+  <section id = "hero">
+    <img id = "cathodictv" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt="Cathodic-Ray TV">
+    <h2 id = "retrofeel">For a nice retro feel, check out our 4K 140FPS<br>gaming screens based on the design<br>of old Cathodic Ray TVs.</h2>
+  </section>
+  <!-- For Sale -->
+  <section id = "forsale">
+    <article id = "product1" class = "product">
+      <div class = productext>
+        <p class = "Description">Config config config config config config<br></p>
+        <p class = "prix">300€<br></p>
+        <p class = "stock">Disponible</p>
+      </div>
+      <img id = "product1img" class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt="Cathodic-Ray TV">
+    </article>
+    <article id = "product2" class = "product">
+      <div class = productext>
+        <p class = "Description">Config config config config config config<br></p>
+        <p class = "prix">300€<br></p>
+        <p class = "stock">Disponible</p>
+      </div>
+      <img id = "productimg" class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt = "Cathodic-Ray TV">
+    </article>
+    <article id = "product3" class = "product">
+      <div class = productext>
+        <p class = "Description">Config config config config config config<br></p>
+        <p class = "prix">300€<br></p>
+        <p class = "stock">Disponible</p>
+      </div>
+      <img id = "product1img" class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt = "Cathodic-Ray TV">
+    </article>
+  </section>
+</main>
+<?php include 'ressources/templates/footer.php'?>
+  <script src = "crg.js"></script>
+</body>
+</html>
---
+++ ressources/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8"> <meta name="viewport" content="width=device-width">
+  <title>Cathodic Ray Gaming</title>
+  <style>
+  <?php include 'crg.css'; ?>
+  </style>
+  <link href="crg.css" rel="stylesheet">
+</head>
+<body onresize = "adaptheight()">
+<header>
+    <nav>
+      <div>
+        <a href = "https://cathodicraygaming.000webhost.com">Cathodic Ray Gaming</a>
+      </div>
+      <div id="nav2">
+        <a href = "https://www.labonnegraine.com/">Login</a>
+        <a href = "https://www.labonnegraine.com/">Sign-up</a>
+        <a href = "https://www.labonnegraine.com/">Account</a>
+        <a href = "https://www.labonnegraine.com/">Panier</a>
+      </div>
+    </nav>
+  </header>
