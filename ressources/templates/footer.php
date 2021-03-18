de6af69bb77a3e9933fcf90508418788c540e413
Ughetto Jules <ughetto.jules@gmail.com>
2021-03-18 17:44:44 +0100

    Inscription finie, adaptation au rangement via nouvelle branche

---
+++ ressources/templates/footer.php
@@ -0,0 +1,71 @@
+<footer id="footer" class="top-space">
+
+  <div class="footer1">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-3 widget">
+          <h3 class="widget-title"> <a href="contact.php">Contact</a></h3>
+          <div class="widget-body">
+            <p>06 21 30 41 25<br>
+              <a href="mailto:#">contact@crg.com</a><br>
+              <br>
+              27 bis rue du Progres, 93100 Montreuil
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-3 widget">
+          <h3 class="widget-title">Follow us</h3>
+          <div class="widget-body">
+            <p class="follow-me-icons clearfix">
+              <a href=""><i class="fa fa-twitter fa-2"></i></a>
+              <a href=""><i class="fa fa-dribbble fa-2"></i></a>
+              <a href=""><i class="fa fa-github fa-2"></i></a>
+              <a href=""><i class="fa fa-facebook fa-2"></i></a>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 widget">
+          <h3 class="widget-title">Qui sommes nous ?</h3>
+          <div class="widget-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
+            <p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
+          </div>
+        </div>
+
+      </div> <!-- /row of widgets -->
+    </div>
+  </div>
+
+  <div class="footer2">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 widget">
+          <div class="widget-body">
+            <p class="simplenav">
+
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 widget">
+          <div class="widget-body">
+            <p class="text-middle ">
+              Copyright &copy; 2021, Ughetto Jules & Haton Gregory. Template by <a href="http://gettemplate.com/" rel="designer">gettemplate</a>
+            </p>
+          </div>
+        </div>
+
+      </div> <!-- /row of widgets -->
+    </div>
+  </div>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <script src="assets/js/headroom.min.js"></script>
+  <script src="assets/js/jQuery.headroom.min.js"></script>
+  <script src="assets/js/template.js"></script>
+</footer>
--- ressources/templates/header.php
+++ ressources/templates/header.php
@@ -1,40 +1,45 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-  <meta charset="UTF-8"> 
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width">
   <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
   <title>Cathodic Ray Gaming</title>
   <style>
-    <?php include 'ressources/templates/Progressus/assets/css/bootstrap.min.css';
-    include 'ressources/templates/Progressus/assets/css/bootstrap.min.css';
-    include 'ressources/templates/Progressus/assets/css/font-awesome.min.css';
-    include 'ressources/templates/Progressus/assets/css/bootstrap-theme.css';
-    include 'ressources/templates/Progressus/assets/css/main.css';
-    include 'crg.css' ?>
+    <?php
+    include 'ressources/css/bootstrap.min.css';
+    include 'ressources/css/bootstrap.min.css';
+    include 'ressources/css/font-awesome.min.css';
+    include 'ressources/css/bootstrap-theme.css';
+    include 'ressources/css/main.css';
+    include 'ressources/css/crg.css'
+    ?>
   </style>
 </head>
-<body onresize ="adaptheight()">
-<header class="navbar navbar-inverse navbar-fixed-top headroom container">
-	<!-- Fixed navbar -->
-	<div class="navbar-header">
-			<!-- Button for smallest screens -->
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-		<a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Cathodic Ray Gaming"></a>
-	</div>
-	<nav class="navbar-collapse collapse nav navbar-nav pull-right">
-		<li><a class="active" href="#">Accueil</a></li>
-		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages produits<b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<li><a href="sidebar-left.html">Premier produit</a></li>
-				<li class="active"><a href="sidebar-right.html">Deuxième produit</a></li>
-        <li><a href="zbleble">Troisième produit</a></li>
-			</ul>
-    </li>
-    <li><a href="inscription.php">S'inscrire</a></li>
-    <li><a href="profil.php">Mon Compte</a></li>
-		<li><a href="panier.php">Panier</a></li>
-  </nav><!--/.nav-collapse -->
-	<!-- /.navbar -->
-</header>
+
+<body onresize="adaptheight()">
+  <header class="navbar navbar-inverse navbar-fixed-top headroom container">
+    <!-- Fixed navbar -->
+    <div class="navbar-header">
+      <!-- Button for smallest screens -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Cathodic Ray Gaming"></a>
+    </div>
+    <nav class="navbar-collapse collapse nav navbar-nav pull-right">
+      <li><a class="active" href="index.php">Accueil</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages produits<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#l">Premier produit</a></li>
+          <li class="active"><a href="#">Deuxième produit</a></li>
+          <li><a href="#">Troisième produit</a></li>
+        </ul>
+      </li>
+      <li><a href="inscription.php">S'inscrire</a></li>
+      <li><a href="profil.php">Mon Compte</a></li>
+      <li><a href="panier.php">Panier</a></li>
+    </nav>
+    <!--/.nav-collapse -->
+    <!-- /.navbar -->
+  </header>
