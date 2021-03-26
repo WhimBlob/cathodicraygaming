a2afa7b3aec56d311367fd4c4d6086bdf02bc098
Ughetto Jules <ughetto.jules@gmail.com>
2021-03-26 07:50:18 +0100

    index corrigé

--- index.php
+++ index.php
@@ -34,12 +34,12 @@
       </article>
       <article id = "product3" class = "product">
         <div class = productext>
-          <h3 class = "productname" id = "productname3">Name 3</h3>
+          <h3 class = "productname" id = "productname2">Name 2</h3>
           <p class = "description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed massa vel justo luctus interdum. Nam nec tempor metus. Vivamus bibendum euismod justo, rutrum scelerisque nulla efficitur nec. Sed elit est, aliquet non diam sit amet, faucibus auctor lacus.</p>
           <p class = "prix">300€</p>
           <p class = "stock">Disponible</p>
         </div>
-        <img class = "product3img" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt = "Cathodic-Ray TV">
+        <img id = "product3img" class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt = "Cathodic-Ray TV">
       </article>
     </section>
   </main>
