61253299dda406e8e212c3224a74fd90c0a119e7
Ughetto Jules <ughetto.jules@gmail.com>
2021-03-26 09:25:19 +0100

    UI panier better

--- panier.php
+++ panier.php
@@ -10,45 +10,49 @@
     <article class="col-md-6 col-sm-8 cartarticle">
     <!-- Product Picture -->
       <img class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt="Cathodic-Ray TV">
-    <!-- Product Name -->
-      <h2>"Lorem ipsum dolor sit amet"</h2>
-    <!-- Avalability -->
-      <p>En stock</p>
-    <!-- Dropdown number -->
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          1
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">2</a>
-          <a class="dropdown-item" href="#">3</a>
-          <a class="dropdown-item" href="#">4</a>
+      <div class = "productoutext">
+      <!-- Product Name -->
+        <h2>"Lorem ipsum dolor sit amet"</h2>
+      <!-- Avalability -->
+        <p>En stock</p>
+      <!-- Dropdown number -->
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            1
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">2</a>
+            <a class="dropdown-item" href="#">3</a>
+            <a class="dropdown-item" href="#">4</a>
+          </div>
         </div>
-      </div>
-    <!-- Prix -->
-      <p class="price">€ 149,00</p>
+      <!-- Prix -->
+        <p class="price">€ 149,00</p>
+      </div> 
     </article>
     <div class="greybar"></div>
     <article class="col-md-6 col-sm-8 cartarticle">
     <!-- Product Picture -->
       <img class = "productimg" src="ressources/imgs/157612182_259155989105775_9145309204689973689_n.png" alt="Cathodic-Ray TV">
-    <!-- Product Name -->
-      <h2>"Lorem ipsum dolor sit amet"</h2>
-    <!-- Avalability -->
-      <p>En stock</p>
-    <!-- Dropdown number -->
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          1
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-          <a class="dropdown-item" href="#">2</a>
-          <a class="dropdown-item" href="#">3</a>
-          <a class="dropdown-item" href="#">4</a>
+      <div class = "productoutext">
+      <!-- Product Name -->
+        <h2>"Lorem ipsum dolor sit amet"</h2>
+      <!-- Avalability -->
+        <p>En stock</p>
+      <!-- Dropdown number -->
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            1
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+            <a class="dropdown-item" href="#">2</a>
+            <a class="dropdown-item" href="#">3</a>
+            <a class="dropdown-item" href="#">4</a>
+          </div>
         </div>
+      <!-- Prix -->
+        <p class="price">€ 149,00</p>
       </div>
-    <!-- Prix -->
-      <p class="price">€ 149,00</p>
     </article>
     <div class="greybar"></div>
     <article class="col-md-6 col-sm-8 cartarticle" id="checkout">
